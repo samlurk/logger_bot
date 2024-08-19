5b61f829e08ad79d02130414c6781fa9e8bb7548
Sebastian Marquez <sebastianmarquez6a@gmail.com>
2024-08-19 11:20:45 -0400

    adding database enviroments

--- src/miscellaneous/generateDB.js
+++ src/miscellaneous/generateDB.js
@@ -1,38 +1,51 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
-require('dotenv').config()
+require("dotenv").config();
 
-async function generateTables () {
-  console.log('Trying to create tables if they don\'t already exist')
+async function generateTables() {
+  console.log("Trying to create tables if they don't already exist");
   const loggerDB = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
-    database: 'logger',
+    database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: 5432
-  })
-  loggerDB.on('error', e => {
-    console.error('There was an error while generating the database structure!', e)
-  })
+    port: 5432,
+  });
+  loggerDB.on("error", (e) => {
+    console.error(
+      "There was an error while generating the database structure!",
+      e
+    );
+  });
   try {
-    const msgQuery = await loggerDB.query('CREATE TABLE IF NOT EXISTS messages ( id TEXT PRIMARY KEY, author_id TEXT NOT NULL, content TEXT, attachment_b64 TEXT, ts TIMESTAMPTZ )') // establish messages table
-    const guildQuery = await loggerDB.query('CREATE TABLE IF NOT EXISTS guilds ( id TEXT PRIMARY KEY, owner_id TEXT NOT NULL, ignored_channels TEXT[], disabled_events TEXT[], event_logs JSON, log_bots BOOL, custom_settings JSON )') // establish guilds table
-    console.log(`DB generation completed, ${msgQuery.rowCount > 0 ? 'generated' : 'skipped'} messages table, ${guildQuery.rowCount > 0 ? 'generated' : 'skipped'} guilds table`)
+    const msgQuery = await loggerDB.query(
+      "CREATE TABLE IF NOT EXISTS messages ( id TEXT PRIMARY KEY, author_id TEXT NOT NULL, content TEXT, attachment_b64 TEXT, ts TIMESTAMPTZ )"
+    ); // establish messages table
+    const guildQuery = await loggerDB.query(
+      "CREATE TABLE IF NOT EXISTS guilds ( id TEXT PRIMARY KEY, owner_id TEXT NOT NULL, ignored_channels TEXT[], disabled_events TEXT[], event_logs JSON, log_bots BOOL, custom_settings JSON )"
+    ); // establish guilds table
+    console.log(
+      `DB generation completed, ${
+        msgQuery.rowCount > 0 ? "generated" : "skipped"
+      } messages table, ${
+        guildQuery.rowCount > 0 ? "generated" : "skipped"
+      } guilds table`
+    );
   } catch (e) {
-    console.error('Failed to generate tables for the database', e)
+    console.error("Failed to generate tables for the database", e);
   }
-  loggerDB.end()
+  loggerDB.end();
 }
 
 if (require.main === module) {
   generateTables()
     .then(() => {
-      process.exit(0)
+      process.exit(0);
     })
-    .catch(e => {
-      console.error('generateTables failed', e)
-      process.exit(1)
-    })
+    .catch((e) => {
+      console.error("generateTables failed", e);
+      process.exit(1);
+    });
 }
 
-module.exports.generateTables = generateTables
+module.exports.generateTables = generateTables;
