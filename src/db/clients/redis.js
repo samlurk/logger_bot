eb99de301d7e76d0b94c7bdbf4f88c87b8b18be9
Sebastian Marquez <sebastianmarquez6a@gmail.com>
2024-08-19 23:08:52 -0400

    adding redis enviroments

--- src/db/clients/redis.js
+++ src/db/clients/redis.js
@@ -1,7 +1,10 @@
-const Redis = require('ioredis')
+const Redis = require("ioredis");
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379
-})
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  tls: {},
+});
 
-module.exports = redis
+module.exports = redis;
